96abe34a80e4d7a29414eca7bc2b95a26310b959
eduardoportillo <eportillovirreira@gmail.com>
2021-06-16 01:04:14 -0400

    sing-up teminado con responsive

--- Front-end/JS/form-validate.js
+++ Front-end/JS/form-validate.js
@@ -1,43 +1,57 @@
-let nombre       = document.getElementById('name');
-let apellido     = document.getElementById('apellido');
-let email        = document.getElementById('email');
-let password     = document.getElementById('password');
-let passwordRep  = document.getElementById('repit-password');
-let error        = document.getElementById('error');
+let nombre = document.getElementById("name");
+let apellido = document.getElementById("apellido");
+let email = document.getElementById("email");
+let password = document.getElementById("password");
+let passwordRep = document.getElementById("repit-password");
+let error = document.getElementById("error");
 
-error.style.color = 'red';
+error.style.color = "#ce1212";
 
-let form = document.getElementById('form');
+let form = document.getElementById("form");
 
-form.addEventListener('submit', (evt) => {
-    evt.preventDefault();
-    
-    
-    console.log('Enviando Formulario');
+form.addEventListener("submit", (evt) => {
+  evt.preventDefault();
 
-    let mensajeError = [];
+  let mensajeError = [];
 
-    if(nombre.value === null || nombre.value === ''){
-        mensajeError.push('Ingresa un nombre');
-    }
+  if (nombre.value === null || nombre.value === "") {
+    mensajeError.push("Ingresa un nombre");
+  }
 
-    if(apellido.value === null || apellido.value === ''){
-        mensajeError.push('Ingresa una contraseña');
-    }
+  if (apellido.value === null || apellido.value === "") {
+    mensajeError.push("Ingresa una apellido");
+  }
 
-    if (email.value === null || email.value === '') {
-        mensajeError.push('Ingresa un email');
-    }
+  if (email.value === null || email.value === "") {
+    mensajeError.push("Ingresa un email");
+  }
 
-    if (password.value === null || password.value === '') {
-        mensajeError.push('Ingresa una contraseña');
-    }
+  if (password.value === null || password.value === "") {
+    mensajeError.push("Ingresa una contraseña");
+  }
 
-    if (password.value !== passwordRep.value) {
-        mensajeError.push('la contraseña no son iguales');
-    }
+  // if (password.value !== passwordRep.value) {
+  //     mensajeError.push('la contraseña no son iguales');
+  // }
 
+  error.innerHTML = mensajeError.join(", ");
 
 
-    error.innerHTML = mensajeError.join(', ');
-})
+  if (mensajeError.length === 0) {
+    fetch("http://localhost:3000/singUp", {
+      method: "POST",
+      body: JSON.stringify({
+        "nombre": nombre.value,
+        "apellido": apellido.value,
+        "correo": email.value,
+        "password": password.value
+      })
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      });
+
+    return true;
+  }
+});
