26d17ea11ca0b5c0d29e2d1bb5f996628b09169b
eduardoportillo <52632798+eduardoportillo@users.noreply.github.com>
2021-10-27 18:45:02 -0400

    cambio una linea

--- Back-end/src/database.js
+++ Back-end/src/database.js
@@ -3,7 +3,7 @@ const { Pool } = require('pg');
 const pool = new Pool({
     user: 'postgres',
     host: '127.0.0.1',
-    password: '8450706183',
+    password: '*********',
     database: 'marketplis',
     port: '5432' 
 });
